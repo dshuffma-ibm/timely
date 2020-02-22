38ad6323b0ac2923c8b1bddb952abe64d7822017
DAVID HUFFMAN <dshuffma@us.ibm.com>
2020-02-21 22:09:52 -0500

    better

--- magic.js
+++ magic.js
@@ -56,16 +56,26 @@ let js_version = `{
 	}
 }`;
 let array_missing_comma = '{"hey": ["ab", "one" "two"]}';
+let array_extra_comma = '{"hey": ["ab", "one" "two",]}';
+let array_extra_comma2 = '{"hey": ["ab", "one", "two",,]}';
+let array_extra_comma3 = '{"hey": ["ab", "one", "two",,,,,]}';
+let obj_extra_comma = `{
+	"hey": {
+		"ab": 2,,
+		"cd": 3
+	},,,,,,,,
+}`;
+
 
 let DEBUG = true;
 let test_cases = [
 	missing_comma, nested_obj_missing_comma, nested_obj_missing_comma2, missing_quote, missing_quote2,
 	missing_quote3, missing_quote4, missing_bracket, missing_bracket2, missing_bracket3, missing_bracket4,
-	js_version, array_missing_comma
+	js_version, array_missing_comma, array_extra_comma, array_extra_comma2, array_extra_comma3,
+	obj_extra_comma
 ];
 let results = [];
 for (let i in test_cases) {
-	console.log('!!', test_cases[i]);
 	let fixed = fixIt(test_cases[i]);
 	console.log('final:', fixed);
 	try {
@@ -79,21 +89,17 @@ console.log('\n\n\n', results);
 
 // fix the JSON - or die in the fire of your own making
 function fixIt(str, iter) {
-	/*const lookingForBracket = Symbol('lookingForBracket');
+	const lookingForBracket = Symbol('lookingForBracket');
 	const lookingForKeyStart = Symbol('lookingForKeyStart');
 	const lookingForKeyEnd = Symbol('lookingForKeyEnd');
 	const lookingForColon = Symbol('lookingForColon');
 	const lookingForValueStart = Symbol('lookingForValueStart');
 	const lookingForValueEnd = Symbol('lookingForValueEnd');
-	const lookingForCommaOrEnd = Symbol('lookingForCommaOrEnd');*/
+	const lookingForCommaOrEnd = Symbol('lookingForCommaOrEnd');
+	let prev_char = null;
 
 	console.log('-----------------');
-	let states = [
-		'lookingForBracket', 'lookingForKeyStart', 'lookingForKeyEnd',
-		'lookingForColon', 'lookingForValueStart', 'lookingForValueEnd',
-		'lookingForCommaOrEnd'
-	];
-	let state = 0;
+	let state = lookingForBracket;
 	let parsingANumber = false;
 	let posLastColon = null;
 	let openBrackets = 0;
@@ -112,13 +118,18 @@ function fixIt(str, iter) {
 
 	if (DEBUG) { console.log('\nStart', str); }
 	for (let i = 0; i < str.length; i++) {
-		//console.log('loop', i, str);
 		let c = str[i];
 		ret += c;				// optimistically this character is good to go
 		if (c === ' ') {
 			continue;
 		}
-		if (DEBUG) { console.log(iter, '[' + c + '] char:' + (i + 1) + '/' + str.length + ' state:' + states[state] + ' parsing:' + parsing); }
+		prev_char = ret[ret.length - 2];
+
+		let state_str = state.toString();
+		state_str = state_str.substr(7);
+		state_str = state_str.substring(0, state_str.length - 1);
+
+		if (DEBUG) { console.log(iter, '[' + c + '] char:' + (i + 1) + '/' + str.length + ' state:' + state_str + ' parsing:' + parsing); }
 
 		if (c === '\\') {
 			escaped_character = true;
@@ -126,72 +137,77 @@ function fixIt(str, iter) {
 		}
 
 		// [lookingForBracket]
-		if (states[state] === 'lookingForBracket') {
+		if (state === lookingForBracket) {
 			if (c === '{' || c === '[') {
 				if (c === '[') {
 					parsing = 'arr';
 				} else {
 					parsing = 'obj';
 				}
-				state++;
+				state = lookingForKeyStart;
 				openBrackets++;
 			} else {
 				if (DEBUG) { console.log('detected missing bracket 1'); }
 				ret = ret.substr(0, ret.length - 1) + '{';
-				state = 1;			// lookingForKeyStart
+				state = lookingForKeyStart;
 				i--;				// repeat
-				//let fixedStr = str.substring(0, i) + '{' + str.substring(i);
-				//return fixIt(fixedStr, iter);
 			}
 		}
 
 		// [lookingForKeyStart]
-		else if (states[state] === 'lookingForKeyStart') {
+		else if (state === lookingForKeyStart) {
 			if (c === '"') {			// its a string
-				state++;
+				state = lookingForKeyEnd;
 			} else if (isStrictLetter(c)) {
 				if (DEBUG) { console.log('detected missing quote 1'); }
 				ret = ret.substr(0, ret.length - 1) + '"';
-				state = 2;			// lookingForKeyEnd
+				state = lookingForKeyEnd;
 				i--;				// repeat
-				//let fixedStr = str.substring(0, i) + '"' + str.substring(i);
-				//return fixIt(fixedStr, iter);
+			} else if (c === ']' || c === '}') {
+				if (DEBUG) { console.log('unexpected bracket, must be extra comma 1'); }
+				parsing = null;
+				ret = ret.substr(0, ret.length - 2);
+				state = lookingForCommaOrEnd;
+				i--;				// repeat
+			} else if (c === ',') {
+				if (DEBUG) { console.log('unexpected comma, must be extra comma 2'); }
+				parsing = null;
+				ret = ret.substr(0, ret.length - 1);
+				state = lookingForKeyStart;
 			}
 		}
 
 		// [lookingForKeyEnd]
-		else if (states[state] === 'lookingForKeyEnd') {
+		else if (state === lookingForKeyEnd) {
 			if (c === '"') {
 				if (parsing === 'arr') {
-					state = 6;					// lookingForCommaOrEnd
+					state = lookingForCommaOrEnd;
 				} else {
-					state++;
+					state = lookingForColon;
 				}
 			} else if (c === ':') {
 				if (DEBUG) { console.log('detected missing quote 2'); }
 				ret = ret.substr(0, ret.length - 1) + '"';
-				state = 3;			// lookingForColon
+				state = lookingForColon;
 				i--;				// repeat
-				//let fixedStr = str.substring(0, i) + '"' + str.substring(i);
-				//return fixIt(fixedStr, iter);
 			} else if (c === '}') {
 				if (DEBUG) { console.log('detected missing quote 6'); }
 				ret = ret.substr(0, ret.length - 1) + '"';
-				state = 6;			// lookingForCommaOrEnd
+				state = lookingForCommaOrEnd;
 				i--;				// repeat
 			}
 		}
 
 		// [lookingForColon]
-		else if (states[state] === 'lookingForColon') {
+		else if (state === lookingForColon) {
 			if (c === ':') {
-				state++;
+				state = lookingForValueStart;
 				posLastColon = ret.length - 1;
 			}
 		}
 
 		// [lookingForValueStart]
-		else if (states[state] === 'lookingForValueStart') {
+		else if (state === lookingForValueStart) {
 			if (c === '[') {
 				parsing = 'arr';
 			} else if (c === '{') {
@@ -199,69 +215,67 @@ function fixIt(str, iter) {
 			}
 
 			if (c === '"') {
-				state++;
+				state = lookingForValueEnd;
 			} else if (isStrictNumber(c)) {				// its a number
-				state++;
+				state = lookingForValueEnd;
 				parsingANumber = true;
 			} else if (c === '{') {						// its an object
-				state = 1;								// lookingForKeyStart
+				state = lookingForKeyStart;
 				openBrackets++;
 			} else if (isStrictLetter(c) || c === '\'') {
 				if (DEBUG) { console.log('detected missing quote 3'); }
 				ret = ret.substr(0, ret.length - 1) + '"';
-				state = 2;			// lookingForKeyEnd
+				state = lookingForKeyEnd;
 				i--;				// repeat
-				//let fixedStr = str.substring(0, i) + '"' + str.substring(i);
-				//return fixIt(fixedStr, iter);
 			}
 		}
 
 		// [lookingForValueEnd]
-		else if (states[state] === 'lookingForValueEnd') {
+		else if (state === lookingForValueEnd) {
 			if (c === '"' && escaped_character === false) {
-				state++;
+				state = lookingForCommaOrEnd;
 			} else if (parsingANumber) {
 				if (!isStrictNumber(c)) {				// end of number reached
-					state++;
+					state = lookingForCommaOrEnd;
 					//i--;
 					parsingANumber = false;
-					if (states[state] === 'lookingForCommaOrEnd') {
+					if (state === lookingForCommaOrEnd) {
 						if (c === '}' || c === ']') {
-							if (DEBUG) { console.log('all done 2, success'); }
-							return ret;
+							state = lookingForCommaOrEnd;
+							ret = ret.substr(0, ret.length - 1);
+							i--;				// repeat
 						}
 					}
 				}
 			} else if (c === ',') {
 				if (DEBUG) { console.log('detected missing quote 4'); }
 				ret = ret.substr(0, ret.length - 1) + '"';
-				state = 3;			// lookingForCommaOrEnd
+				state = lookingForCommaOrEnd;
 				i--;				// repeat
-				//let fixedStr = str.substring(0, i) + '"' + str.substring(i);
-				//return fixIt(fixedStr, iter);
 			} else if (c === '}') {
 				if (DEBUG) { console.log('detected missing quote 5'); }
 				ret = ret.substr(0, ret.length - 1) + '"';
-				state = 6;			// lookingForCommaOrEnd
+				state = lookingForCommaOrEnd;
 				i--;				// repeat
-				//let fixedStr = str.substring(0, i) + '"' + str.substring(i);
-				//return fixIt(fixedStr, iter);
 			}
 		}
 
 		// [lookingForCommaOrEnd]
-		else if (states[state] === 'lookingForCommaOrEnd') {
+		else if (state === lookingForCommaOrEnd) {
 			if (c === '"') {
 				if (DEBUG) { console.log('detected missing comma 1'); }
 				ret = ret.substr(0, ret.length - 1) + ',';
-				state = 1;			// lookingForKeyStart
+				state = lookingForKeyStart;
 				i--;				// repeat
-				//let fixedStr = str.substring(0, i) + ',' + str.substring(i);
-				//return fixIt(fixedStr, iter);
 			} else if (c === ',') {
-				state = 1;			// lookingForKeyStart
+				if (prev_char === ',') {
+					if (DEBUG) { console.log('unexpected comma, must be extra comma 3'); }
+					ret = ret.substr(0, ret.length - 1);
+					state = lookingForKeyStart;
+				} else {
+					state = lookingForKeyStart;
+				}
 			} else if (c === '}') {
-				//console.log('?', i, str.length, '[' + str.substring(i) + ']');
 				openBrackets--;
 				parsing = null;
 				if (i < str.length - 1) {
@@ -277,30 +291,18 @@ function fixIt(str, iter) {
 						if (DEBUG) { console.log('detected missing bracket 4'); }
 						ret += '}';
 					}
-					/*if (openBrackets > 0) {
-						if (DEBUG) { console.log('detected missing bracket 4'); }
-						let fixedStr = str.substring(0, i) + '}' + str.substring(i);
-						return fixIt(fixedStr, iter);
-					} else {*/
 					if (DEBUG) { console.log('all done 1, success'); }
 					return ret;
-					//}
 				}
 			} else if (c === ':') {
 				// this fix is a guess... the error is detected way after it happened
 				if (DEBUG) { console.log('detected missing bracket in past'); }
 				ret = str.substring(0, posLastColon + 1) + '{' + str.substring(posLastColon + 1);
-				state = 1;			// lookingForKeyStart
+				state = lookingForKeyStart;
 				return fixIt(ret, iter);				// repeat...
-				//let fixedStr = str.substring(0, posLastColon + 1) + '{' + str.substring(posLastColon + 1);
-				//return fixIt(fixedStr, iter);
 			} else if (c === ']') {
 				parsing = null;
-				state = 6;			// lookingForCommaOrEnd
-			} else {
-				if (DEBUG) { console.log('detected missing bracket 2'); }
-				//let fixedStr = str.substring(0, i) + '}' + str.substring(i);
-				//return fixIt(fixedStr, iter);
+				state = lookingForCommaOrEnd;
 			}
 		}
 
@@ -311,8 +313,6 @@ function fixIt(str, iter) {
 	if (DEBUG) { console.log('detected missing bracket 3'); }
 	ret += '}';
 	return ret;
-	//let fixedStr = str + '}';
-	//return fixIt(fixedStr, iter);
 }
 
 function isStrictNumber(char) {
