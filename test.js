1011b44b9d90706f686b6110d15aa22499d7d8a1
DAVID HUFFMAN <dshuffma@us.ibm.com>
2020-02-27 21:38:06 -0500

    check next line

--- test.js
+++ test.js
@@ -1,7 +1,9 @@
 
-const textInput1 = '{hey": "\'there",\n "test":"me", \n"a":["same"], \n"abc"test" \n "d": "e",\n "abc":"def"}';
-const textInput2 = '{"hey": "there", \n"test":"me", \n"a":["same"], \n"abc":"tet" \n "d": "e",\n "ghi": "jkl"}';
+//const textInput1 = '{hey": "\'there",\n "test":"me", \n"a":["same"], \n"abc"test" \n "d": "e",\n "abc":"def"}';
+//const textInput2 = '{"hey": "there", \n"test":"me", \n"a":["same"], \n"abc":"tet" \n "d": "e",\n "ghi": "jkl"}';
 
+const textInput1 = '{hey": "\'there",\n "test":"me", \n"abc"test" \n "d": "e",\n "abc":"def"}';
+const textInput2 = '{"hey": "there", \n"test":"me", \n"a":["same"], \n"abc":"tet" \n "d": "e",\n "ghi": "jkl"}';
 document.querySelector('#inputText').innerHTML = breakMeDown(textInput1, textInput2);
 
 
@@ -11,14 +13,40 @@ function breakMeDown(orig_str, fixed_str) {
 	let ret = '';
 	const orig_lines = orig_str.split('\n');
 	const fixed_lines = fixed_str.split('\n');
-	for (let i in orig_lines) {
-		if (orig_lines[i] === fixed_lines[i]) {
+	let x = 0;
+	for (let i = 0; i < orig_lines.length; i++) {
+		if (orig_lines[i] === fixed_lines[x]) {
 			ret += '<sss>🗸 ' + orig_lines[i] + '</sss>\n';
 		} else {
-			ret += '<ogl>- ' + findDeletions(orig_lines[i], fixed_lines[i]) + '</ogl>\n';
-			ret += '<edl>+ ' + findAdditions(orig_lines[i], fixed_lines[i]) + '</edl>\n';
+			const del = findDeletions(orig_lines[i], fixed_lines[x]);
+			const add = findAdditions(orig_lines[i], fixed_lines[x]);
+			const total = del.differences + add.differences;
+
+			let total2 = null;
+			let del2, add2;
+			if (fixed_lines[Number(x) + 1]) {	// check if the next line is actually better
+				del2 = findDeletions(orig_lines[i], fixed_lines[Number(x) + 1]);
+				add2 = findAdditions(orig_lines[i], fixed_lines[Number(x) + 1]);
+				total2 = del2.differences + add2.differences;
+			}
+
+			if (total2 !== null && total2 < total) {
+				console.log('the next line is a better match');
+				let blank = '';
+				for (let z = 0; z < add.str.length; z++) { blank += ' '; }
+				ret += '<ogl>-<diff>' + blank + '</diff></ogl>\n';
+				ret += '<edl>+ ' + add.str + '</edl>\n';
+				ret += '\n';
+				ret += '<ogl>- ' + del2.str + '</ogl>\n';
+				ret += '<edl>+ ' + add2.str + '</edl>\n';
+				x++;
+			} else {
+				ret += '<ogl>- ' + del.str + '</ogl>\n';
+				ret += '<edl>+ ' + add.str + '</edl>\n';
+			}
 		}
 		ret += '\n';
+		x++;
 	}
 	return ret;
 }
@@ -27,9 +55,13 @@ function breakMeDown(orig_str, fixed_str) {
 function findAdditions(orig_line, fixed_line) {
 	console.log('findAdditions ol', orig_line);
 	console.log('findAdditions fl', fixed_line);
+	let differences = 0;
 	let ret = '';
 	let x = 0;
 	let detectedDiff = false;
+	orig_line = orig_line ? orig_line : '';
+	fixed_line = fixed_line ? fixed_line : '';
+
 	for (let i = 0; i < fixed_line.length; i++) {
 		console.log(i, x, fixed_line[i], orig_line[x], ret, detectedDiff);
 
@@ -40,6 +72,7 @@ function findAdditions(orig_line, fixed_line) {
 			}
 			ret += fixed_line[i];
 		} else {
+			differences++;
 			if (fixed_line[i + 1] === orig_line[x]) {
 				// if the next character in fixed does match this orig character, than this char was added (back on track)
 				// probably at least
@@ -48,7 +81,7 @@ function findAdditions(orig_line, fixed_line) {
 					ret += '<dif>';
 				}
 				ret += fixed_line[i];
-				x--;							// repeat
+				x--;							// repeat char in original
 			} else if (fixed_line[i] === orig_line[x + 1]) {
 				// if the next character in orig matches this fixed character, than this char was deleted (back on track)
 				// probably at least
@@ -60,20 +93,24 @@ function findAdditions(orig_line, fixed_line) {
 					ret += '<dif>';
 				}
 				ret += fixed_line[i];
-				x--;							// repeat
+				x--;							// repeat char in original
 			}
 		}
 		x++;
 	}
-	return ret;
+	return { str: ret, differences: differences };
 }
 
 function findDeletions(orig_line, fixed_line) {
 	console.log('findDeletions ol', orig_line);
 	console.log('findDeletions fl', fixed_line);
+	let differences = 0;
 	let ret = '';
 	let x = 0;
 	let detectedDiff = false;
+	orig_line = orig_line ? orig_line : '';
+	fixed_line = fixed_line ? fixed_line : '';
+
 	for (let i = 0; i < orig_line.length; i++) {
 		console.log(i, x, orig_line[i], fixed_line[x], ret, detectedDiff);
 
@@ -84,6 +121,7 @@ function findDeletions(orig_line, fixed_line) {
 			}
 			ret += orig_line[i];
 		} else {
+			differences++;
 			if (orig_line[i] === fixed_line[x + 1]) {
 				// if the next character in fixed does match this orig character, than this char was added (back on track)
 				// probably at least
@@ -97,7 +135,7 @@ function findDeletions(orig_line, fixed_line) {
 					ret += '<dif>';
 				}
 				ret += orig_line[i];
-				x--;							// repeat
+				x--;							// repeat char in fixed
 			} else {
 				if (orig_line[i]) { ret += orig_line[i]; }
 				x++;							// skip added char
@@ -105,5 +143,5 @@ function findDeletions(orig_line, fixed_line) {
 		}
 		x++;
 	}
-	return ret;
+	return { str: ret, differences: differences };
 }
