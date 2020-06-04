4afe3445fc526331987b80a3c4c88a6ec5cf5e1c
DAVID HUFFMAN <dshuffma@us.ibm.com>
2020-06-03 20:55:54 -0400

    ignore whitespace in diff and break lines by items

--- experiments/test.js
+++ experiments/test.js
@@ -7,7 +7,7 @@
 
 //const textInput1 = '{hey": "\'there",\n "test":"me", \n"a":["same"], \n"abc"test" \n "d": "e",\n "abc":"def"}';
 //const textInput2 = '{"hey": "there", \n"test":"me", \n"a":["same"], \n"abc":"tet" \n "d": "e",\n "aec": "deg"}';
-
+/*
 const textInput1 = `{
     "hey": {
         "boo": [
@@ -48,31 +48,64 @@ const textInput2 = `{
 		]
 	}
 }`;
-document.querySelector('#inputText').innerHTML = breakMeDown(textInput1, textInput2);
+*/
 
-function break_after(str, characters) {
-	const ret = [];
-	for (let i = 0; i < str.length - 1;) {
-		ret.push(str.substring(i, i + characters));
-		i += characters;
+const textInput1 = `{
+	"values": {
+		"MSP": {
+			"mod_policy": "Admins",
+			"value": 
+				"config": {
+					"admins": [
+						"sadf"
+					],
+					"crypto_config": null,
+					"fabric_node_ous": null,
+					"intermediate_certs": [
+						"asdf=",
+						"sfsaf="
+					],
+					"name": "PeerOrg1",
+					"organizational_unit_identifiers": [
+					],
+					"revocation_list": [
+					],
+					"root_certs": [
+						"asdf="
+					],
+					"signing_identity": null,
+					"tls_intermediate_certs": [
+						"adsf",
+						"asdf"
+					],
+					"tls_root_certs": [
+						"dfs"
+					]
+				},
+				"type": 0
+			}
+		}
 	}
-	return ret;
-}
+}`;
+
+const textInput2 = `{"val ues" : { "MSP" : { "mod_policy": "Admins"  , "value" : {"config":{"admins" : ["sadf"],"crypto_config":null,"fabric_node_ous":null,"intermediate_certs":["asdf=","sfsaf="],"name":"PeerOrg1","organizational_unit_identifiers":[],"revocation_list":[],"root_certs":["asdf="],"signing_identity":null,"tls_intermediate_certs":["adsf","asdf"],"tls_root_certs":["dfs"]},"type":0}}}}`;
+
+document.querySelector('#inputText').innerHTML = breakMeDown(textInput1, textInput2);
 
 // pretty print json
 function breakMeDown(orig_str, fixed_str) {
 	let ret = '';
-	const LEN = 90;
-	const orig_lines = break_after(orig_str.replace(/[\t\n\r]/g, '').replace(/\s*([^"])/g, '$1').replace(/[{[]\s+"/g, '{"'), LEN);
-	const fixed_lines = break_after(fixed_str.replace(/[\t\n\r]/g, '').replace(/\s*([^"])/g, '$1').replace(/[{[]\s+"/g, '{"'), LEN);
+	const orig_lines = break_items(orig_str.replace(/[\t\n\r]/g, '').replace(/\s*([^"])/g, '$1').replace(/[{]\s+"/g, '{"'));
+	const fixed_lines = break_items(fixed_str.replace(/[\t\n\r]/g, '').replace(/\s*([^"])/g, '$1').replace(/[{]\s+"/g, '{"'));
 	console.log(orig_lines);
 	console.log(fixed_lines);
 
 	let x = 0;
 	for (let i = 0; i < orig_lines.length; i++) {
-		orig_lines[i] = orig_lines[i].trim();
-		fixed_lines[x] = fixed_lines[x].trim();
-		if (orig_lines[i] === fixed_lines[x]) {
+		if (orig_lines[x]) { orig_lines[i] = orig_lines[i].trim(); }
+		if (fixed_lines[x]) { fixed_lines[x] = fixed_lines[x].trim(); }
+		//if (orig_lines[i] === fixed_lines[x]) {
+		if (orig_lines[i].replace(/:\s+/g, ':') === fixed_lines[x].replace(/:\s+/g, ':')) {		// ignore space differences
 			ret += '<sss>🗸 ' + orig_lines[i] + '</sss>\n';
 		} else {
 			const del = findDeletions(orig_lines[i], fixed_lines[x]);
@@ -105,7 +138,55 @@ function breakMeDown(orig_str, fixed_str) {
 	return ret;
 }
 
-//					other line, print-this-line
+function break_after(str) {
+	const LEN = 90;						// really old way
+	const ret = [];
+	for (let i = 0; i < str.length - 1;) {
+		ret.push(str.substring(i, i + LEN));
+		i += LEN;
+	}
+}
+
+function break_items(str) {
+	/*const ret = str.split('",');		// old way
+	for (let i in ret) {
+		ret[i] = ret[i] += '",';
+	}*/
+
+	const ret = [];
+	let iter = 0;
+	while (iter <= 1000) {
+		iter++;
+		const pos = indexOfThings(str);
+		if (pos >= 0) {
+			const before = str.substring(0, pos);
+			const after = str.substring(pos);
+			ret.push(before);
+			str = after;
+		} else {
+			ret.push(str);
+			break;
+		}
+	}
+	return ret;
+
+	function indexOfThings(str) {
+		const split_on = ['",', 'false,', 'true,', 'null,', '},', '],'];
+		let ret = -1;
+		for (let i in split_on) {
+			const thing = split_on[i];
+			let pos = str.indexOf(thing);
+			if (pos >= 0) {
+				pos += thing.length
+				if (ret === -1 || (pos < ret)) {
+					ret = pos;
+				}
+			}
+		}
+		return ret;
+	}
+}
+
 function findAdditions(orig_line, fixed_line) {
 	console.log('findAdditions ol', orig_line);
 	console.log('findAdditions fl', fixed_line);
@@ -124,6 +205,9 @@ function findAdditions(orig_line, fixed_line) {
 				detectedDiff = false;
 				ret += '</dif>';
 			}
+			ret += fixed_line[i];
+		} else if (fixed_line[i] === ' ') {		// skip the highlighting of space differences (this will hide space differences that matter, like in values or keys)
+			x--;								// should be okay b/c json fixer should not be injecting spaces in keys or values to make such a differences
 			ret += fixed_line[i];
 		} else {
 			differences++;
@@ -186,6 +270,9 @@ function findDeletions(orig_line, fixed_line) {
 				detectedDiff = false;
 				ret += '</dif>';
 			}
+			ret += orig_line[i];
+		} else if (orig_line[i] === ' ') {			// skip the highlighting of space differences (this will hide space differences that matter, like in values or keys)
+			x--;
 			ret += orig_line[i];
 		} else {
 			differences++;
--- popup/actions.js
+++ popup/actions.js
@@ -468,19 +468,21 @@ function fixIt(str, iter, maxIter) {
 // ------------------------------------------------------
 // pretty print diff of two strings
 // ------------------------------------------------------
+
+// pretty print json
 function breakMeDown(orig_str, fixed_str) {
 	let ret = '';
-	const LEN = 90;
-	const orig_lines = break_after(orig_str.replace(/[\t\n\r]/g, '').replace(/\s*([^"])/g, '$1').replace(/[{[]\s+"/g, '{"'), LEN);
-	const fixed_lines = break_after(fixed_str.replace(/[\t\n\r]/g, '').replace(/\s*([^"])/g, '$1').replace(/[{[]\s+"/g, '{"'), LEN);
+	const orig_lines = break_items(orig_str.replace(/[\t\n\r]/g, '').replace(/\s*([^"])/g, '$1').replace(/[{]\s+"/g, '{"'));
+	const fixed_lines = break_items(fixed_str.replace(/[\t\n\r]/g, '').replace(/\s*([^"])/g, '$1').replace(/[{]\s+"/g, '{"'));
 	console.log(orig_lines);
 	console.log(fixed_lines);
 
 	let x = 0;
 	for (let i = 0; i < orig_lines.length; i++) {
-		orig_lines[i] = orig_lines[i].trim();
-		fixed_lines[x] = fixed_lines[x].trim();
-		if (orig_lines[i] === fixed_lines[x]) {
+		if (orig_lines[x]) { orig_lines[i] = orig_lines[i].trim(); }
+		if (fixed_lines[x]) { fixed_lines[x] = fixed_lines[x].trim(); }
+		//if (orig_lines[i] === fixed_lines[x]) {
+		if (orig_lines[i].replace(/:\s+/g, ':') === fixed_lines[x].replace(/:\s+/g, ':')) {		// ignore space differences
 			ret += '<sss>🗸 ' + orig_lines[i] + '</sss>\n';
 		} else {
 			const del = findDeletions(orig_lines[i], fixed_lines[x]);
@@ -513,17 +515,57 @@ function breakMeDown(orig_str, fixed_str) {
 	return ret;
 }
 
-// break each line into equal parts
-function break_after(str, characters) {
+function break_after(str) {
+	const LEN = 90;						// really old way
 	const ret = [];
 	for (let i = 0; i < str.length - 1;) {
-		ret.push(str.substring(i, i + characters));
-		i += characters;
+		ret.push(str.substring(i, i + LEN));
+		i += LEN;
 	}
-	return ret;
 }
 
-//					other line, print-this-line
+function break_items(str) {
+	/*const ret = str.split('",');		// old way
+	for (let i in ret) {
+		ret[i] = ret[i] += '",';
+	}*/
+
+	const ret = [];
+	let iter = 0;
+	while (iter <= 1000) {
+		iter++;
+		const pos = indexOfThings(str);
+		if (pos >= 0) {
+			const before = str.substring(0, pos);
+			const after = str.substring(pos);
+			ret.push(before);
+			str = after;
+		} else {
+			ret.push(str);
+			break;
+		}
+	}
+	console.log('?', ret);
+	return ret;
+
+	function indexOfThings(str) {
+		const split_on = ['",', 'false,', 'true,', 'null,', '},', '],'];
+		let ret = -1;
+		for (let i in split_on) {
+			const thing = split_on[i];
+			let pos = str.indexOf(thing);
+			if (pos >= 0) {
+				pos += thing.length
+				if (ret === -1 || (pos < ret)) {
+					ret = pos;
+					//return pos + thing.length;
+				}
+			}
+		}
+		return ret;
+	}
+}
+
 function findAdditions(orig_line, fixed_line) {
 	console.log('findAdditions ol', orig_line);
 	console.log('findAdditions fl', fixed_line);
@@ -531,8 +573,8 @@ function findAdditions(orig_line, fixed_line) {
 	let ret = '';
 	let x = 0;
 	let detectedDiff = false;
-	orig_line = orig_line ? orig_line.trim() : '';
-	fixed_line = fixed_line ? fixed_line.trim() : '';
+	orig_line = orig_line ? orig_line : '';
+	fixed_line = fixed_line ? fixed_line : '';
 
 	for (let i = 0; i < fixed_line.length; i++) {
 		console.log(i, x, fixed_line[i], orig_line[x], ret, detectedDiff);
@@ -542,6 +584,9 @@ function findAdditions(orig_line, fixed_line) {
 				detectedDiff = false;
 				ret += '</dif>';
 			}
+			ret += fixed_line[i];
+		} else if (fixed_line[i] === ' ') {		// skip the highlighting of space differences (this will hide space differences that matter, like in values or keys)
+			x--;								// should be okay b/c json fixer should not be injecting spaces in keys or values to make such a differences
 			ret += fixed_line[i];
 		} else {
 			differences++;
@@ -604,6 +649,9 @@ function findDeletions(orig_line, fixed_line) {
 				detectedDiff = false;
 				ret += '</dif>';
 			}
+			ret += orig_line[i];
+		} else if (orig_line[i] === ' ') {			// skip the highlighting of space differences (this will hide space differences that matter, like in values or keys)
+			x--;
 			ret += orig_line[i];
 		} else {
 			differences++;
